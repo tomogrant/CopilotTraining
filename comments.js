ec574fdcd1c58e6c12293dc1f9c5d203aa474f45
Tom Grant <159554972+tomogrant@users.noreply.github.com>
2024-04-23 14:16:22 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,11 +1,38 @@
-const http = require('http');
+// Create web server
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const Comment = require('./models/comment');
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, world!');
+// Connect to mongodb
+mongoose.connect('mongodb://localhost:27017/comments');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Get all comments
+app.get('/comments', (req, res) => {
+  Comment.find({}, (err, comments) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(comments);
+    }
+  });
 });
 
-server.listen(3000, 'localhost', () => {
-    console.log('Server running at http://localhost:3000/');
+// Get comment by id
+app.get('/comments/:id', (req, res) => {
+  Comment.findById(req.params.id, (err, comment) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(comment);
+    }
+  });
 });
+
+// Post new comment
+app.post('/comments', (req, res) => {
+  const comment = new Comment({
